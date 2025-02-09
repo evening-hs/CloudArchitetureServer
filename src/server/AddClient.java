fec5f5ad467924040154a6fbc5f9c6e0bddc451d
Evelyn Huerta <evelynhs400@gmail.com>
2025-02-09 00:44:09 -0600

    Intento de envíar mensajes a los demás clientes.
    No funciona aún, se queda esperando un mensaje.
    
    Debe haber un problema en la manera en que estoy compartiendo el los DataStream, probablemente se está cerrando?

--- src/server/AddClient.java
+++ src/server/AddClient.java
@@ -1,6 +1,5 @@
 package server;
 
-//import org.json.JSONObject;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -21,33 +20,26 @@ public class AddClient extends Thread {
     @Override
     public void run() {
         String received;
-        while (true) {
-            try {
-                dos.writeUTF("Add username");
-                received = dis.readUTF();
-                //json
-                if (received.equals("Exit")) {
-                    System.out.println("Client " + socket + " sends Exit...");
-                    System.out.println("Closing the connection.");
-                    socket.close();
-                    System.out.println("Connection closed.");
-                    break;
-                }
-                System.out.println(received);
-
-                ClientManager client = new ClientManager(socket.getInetAddress(), socket.getPort() , received);
-                Server.connectedClients.add(client);
-            } catch (Exception e) {
-                System.out.println(e);
-            }
-        }
         try {
-            dis.close();
-            dos.close();
-        } catch (IOException e) {
-            System.out.println("Error: " + e);
-        }
+            dos.writeUTF("Add username");
+            received = dis.readUTF();
+            //json
+            if (received.equals("Exit")) {
+                System.out.println("Client " + socket + " sends Exit...");
+                System.out.println("Closing the connection.");
+                socket.close();
+                System.out.println("Connection closed.");
+            }
+            System.out.println(received);
 
+            ClientManager client = new ClientManager(
+                    socket.getInetAddress(),socket.getPort(),
+                    received,socket, dis, dos);
+            client.run();
+            Server.connectedClients.add(client);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
 }
--- src/server/ClientManager.java
+++ src/server/ClientManager.java
@@ -1,6 +1,12 @@
 package server;
 
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.net.InetAddress;
 
@@ -9,12 +15,50 @@ public class ClientManager {
     public InetAddress ip;
     public int port;
     public String username;
-    public ConcurrentLinkedQueue<JSONObject> queue = new ConcurrentLinkedQueue<>();
+    public ConcurrentLinkedQueue<JSONObject> queue
+            = new ConcurrentLinkedQueue<>();
+    private Socket socket;
+    private DataInputStream dis;
+    private DataOutputStream dos;
 
-    public ClientManager(InetAddress ip, int port, String username) {
+    public ClientManager(InetAddress ip, int port, String username,
+                         Socket socket, DataInputStream dis,
+                         DataOutputStream dos) throws IOException {
         this.ip = ip;
         this.port = port;
         this.username = username;
+        this.socket = socket;
+        this.dis = dis;
+        this.dos = dos;
+    }
+
+    public void run() {
+        Thread senderThread = new Thread(() -> {
+            try {
+                sender();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Thread receiverThread = new Thread(this::receiver);
+        senderThread.start();
+        receiverThread.start();
+    }
+
+    public void receiver() {
+        for (;;) {
+            try {
+                JSONObject received = new JSONObject(this.dis.readUTF());
+                // FIXME no está recibiendo nada aquí
+                Server.queue.add(received);
+                System.out.println("Server received:\n" + received);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (JSONException e) {
+                System.err.println("Client sent a message with invalid JSON: "
+                        + e);
+            }
+        }
     }
 
     public void sender() throws InterruptedException {
@@ -26,7 +70,15 @@ public class ClientManager {
             if (message == null)
                 continue;
 
-            // mandar al socket 👍
+            System.out.println("Sending message\n" + message);
+            try {
+                this.dos.writeUTF(message.toString());
+            } catch (IOException e) {
+                System.err.println("Cannot send message\n: " + message);
+                // Add the message back to the queue so we can try to send it
+                // again
+                this.queue.add(message);
+            }
         }
     }
 }
